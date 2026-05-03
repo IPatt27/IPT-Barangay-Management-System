e962f97e5d2348b5d7ea36bfccb8eb36a02c0921
Pula <alred1420@gmail.com>
2026-05-03 20:40:09 +0800

    Create Official model with fillable and casts

---
+++ app/Models/Official.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Official extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'position',
+        'designation',
+        'contact',
+        'address',
+        'birthdate',
+        'term_start',
+        'term_end',
+        'photo',
+        'status',
+    ];
+
+    protected $casts = [
+        'birthdate'  => 'date',
+        'term_start' => 'date',
+        'term_end'   => 'date',
+    ];
+
+    public function getFullNameAttribute()
+    {
+        return $this->first_name . ' ' . $this->last_name;
+    }
+}
